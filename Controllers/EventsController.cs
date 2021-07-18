85f703fd91651814317e6f0575f6eed6d1ec489a
steven.j.wilcox@gmail.com <steven.j.wilcox@gmail.com>
2021-07-18 17:49:19 -0400

    Exercise complete

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -8,7 +8,7 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private List<string> Events = new List<string>();
+        static private Dictionary<string,string> Events = new Dictionary<string, string>();
         [HttpGet]
         public IActionResult Index()
         {
@@ -24,9 +24,9 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add/")]
-        public IActionResult NewEvent(string name)
+        public IActionResult NewEvent(string name, string description)
         {
-            Events.Add(name);
+            Events.Add(name, description);
             return Redirect("/Events/");
         }
     }
